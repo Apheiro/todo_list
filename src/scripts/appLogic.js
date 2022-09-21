8ac76d45d44a5a297d838d62879b21c817d0de90
Eyen <eyentuyon@gmail.com>
2022-09-21 00:23:51 -0300

    Add list objects, relate them to DOM

--- src/scripts/appLogic.js
+++ src/scripts/appLogic.js
@@ -1,6 +1,9 @@
 import { userInterface } from './domCreation.js'
 
 class appLogic {
+    static lists = [];
+    static listSelected;
+
     static logic() {
         const addListBtn = document.querySelector('#addListBtn');
         const addListForm = document.querySelector('#addListForm');
@@ -8,26 +11,27 @@ class appLogic {
         const addListCancelBtn = document.querySelector('#addListCancelBtn');
         const addListInput = document.querySelector('#addListInput');
 
-        const addItemBtn = document.querySelector('#addItemBtn');
-        const addItemForm = document.querySelector('#addItemForm');
-        const addItemCancelBtn = document.querySelector('#addItemCancelBtn');
+        const addTaskBtn = document.querySelector('#addTaskBtn');
+        const addTaskForm = document.querySelector('#addTaskForm');
+        const addTaskCancelBtn = document.querySelector('#addTaskCancelBtn');
 
-        const editListNameForm = document.querySelector('#editListNameForm')
-        const editListCancelBtn = document.querySelector('#editListCancelBtn')
+        const editListInput = document.querySelector('#editListInput');
+        const editListNameForm = document.querySelector('#editListNameForm');
+        const editListCancelBtn = document.querySelector('#editListCancelBtn');
+        const editListSubmitBtn = document.querySelector('#editListSubmitBtn');
 
         const backgroundOfForms = document.querySelector('#backgroundOfForms');
-        const optionsContent = document.querySelector('#optionsContent');
         const menuContent = document.querySelector('#menuContent');
-        const list = document.querySelector('.list')
 
+        const list = document.querySelector('.list');
 
         addListBtn.addEventListener('click', () => { appLogic.showForm(addListForm, backgroundOfForms) });
 
+        list.addEventListener('click', () => { appLogic.showSelectListContent(list) });
 
-        list.addEventListener('click', () => { listOfTasks.showSelectListContent(list, optionsContent) });
         addListSubmitBtn.addEventListener('click', (e) => {
             if (addListInput.value !== '') {
-                listOfTasks.createList(menuContent, optionsContent, addListInput.value);
+                appLogic.createList(addListInput.value);
                 backgroundOfForms.classList.toggle('h');
                 addListForm.classList.toggle('h');
                 e.preventDefault();
@@ -42,11 +46,26 @@ class appLogic {
             addListForm.reset();
         });
 
-        addItemCancelBtn.addEventListener('click', (e) => {
+        addTaskCancelBtn.addEventListener('click', (e) => {
             backgroundOfForms.classList.toggle('h');
-            addItemForm.classList.toggle('h');
+            addTaskForm.classList.toggle('h');
             e.preventDefault();
-            addItemForm.reset();
+            addTaskForm.reset();
+        });
+
+        editListSubmitBtn.addEventListener('click', (e) => {
+            if (editListInput.value !== '') {
+                this.lists.forEach(element => {
+                    if (element.id === this.listSelected) {
+                        element.title = editListInput.value
+                        this.refreshDataList(element)
+                    }
+                })
+                backgroundOfForms.classList.toggle('h');
+                editListNameForm.classList.toggle('h');
+                e.preventDefault();
+                editListNameForm.reset();
+            }
         });
 
         editListCancelBtn.addEventListener('click', (e) => {
@@ -55,6 +74,51 @@ class appLogic {
             e.preventDefault();
             editListNameForm.reset();
         });
+
+    }
+
+    static refreshDataList(list) {
+        const ListSelected = list.domReference;
+        ListSelected.querySelector('.titleOfList').innerText = `${list.title}`;
+        ListSelected.querySelector('.TasksOfList').innerText = `${list.tasksNumber} Tasks`;
+        ListSelected.querySelector('.TasksCompletedOfList').innerText = `${list.taskCompleted} Completed`;
+    }
+
+    static createList(title) {
+        const newList = new list(title, 0);
+        const elements = userInterface.createListDom(newList.title, newList.tasksNumber, newList.taskCompleted);
+        newList.domReference = elements[0];
+        newList.id = elements[5];
+        this.lists.push(newList)
+
+        elements[0].addEventListener('click', () => { appLogic.showSelectListContent(elements[0]) });
+        elements[0].addEventListener('mouseover', () => { elements[1].classList.add('buttonSettingsShow') });
+        elements[0].addEventListener('mouseout', () => { elements[1].classList.remove('buttonSettingsShow') });
+        elements[2].addEventListener('click', (e) => {
+            appLogic.showForm(editListNameForm, backgroundOfForms);
+            appLogic.listSelected = newList.id;
+            e.stopPropagation();
+        });
+        elements[3].addEventListener('click', (e) => {
+            appLogic.deleteList(newList);
+            console.log(this.lists)
+            e.stopPropagation();
+        });
+        elements[4].addEventListener('click', () => { appLogic.showForm(addTaskForm, backgroundOfForms) });
+
+
+    }
+
+    static showSelectListContent(list) {
+        const menuContent = list.parentElement.childNodes;
+        menuContent.forEach(task => { if (task.classList.contains('active')) { task.classList.remove('active'); }; });
+        if (!list.classList.contains('active')) { list.classList.add('active'); };
+    }
+
+    static deleteList(list) {
+        list.domReference.remove();
+        this.lists.forEach(element => { if (element.id === list.id) { this.lists.splice(this.lists.indexOf(element), 1); } })
+        appLogic.setNewIndex(document.querySelector('#menuContent'), 'data-indexlist')
     }
 
     static showForm(form, background) {
@@ -62,72 +126,42 @@ class appLogic {
         form.classList.toggle('h');
     }
 
-    static matchingIndex(container, dataAtribute) {
+    static setNewIndex(container, dataAtribute) {
         const nodeList = container.childNodes;
-        console.log(nodeList)
-        nodeList.forEach(node => { node.setAttribute(`${dataAtribute}`, appLogic.nodeIndexItem(container, node)); });
+        nodeList.forEach(node => {
+            this.lists.forEach(element => {
+                if (element.id == node.getAttribute('data-indexlist')) {
+                    element.id = appLogic.matchingIndex(container, node)
+                    node.setAttribute(`${dataAtribute}`, element.id);
+                }
+            })
+        });
     }
 
-    static nodeIndexItem(parent, child) {
+    static matchingIndex(parent, child) {
         const index = Array.prototype.indexOf.call(parent.children, child);
         return index;
     }
+
+}
+class task {
+    constructor(status, description, date, priority) {
+        this.status = status;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+        this.checked = false
+    }
 }
 
-class listOfTasks {
-    static createList(menuContent, optionsContent, title) {
-        const elements = userInterface.createListDom(menuContent, optionsContent, title)
-        elements[0].addEventListener('click', () => { listOfTasks.showSelectListContent(elements[0], optionsContent) });
-        elements[0].addEventListener('mouseover', () => { elements[1].classList.add('buttonSettingsShow') })
-        elements[0].addEventListener('mouseout', () => { elements[1].classList.remove('buttonSettingsShow') })
-        elements[2].addEventListener('click', (e) => {
-            listOfTasks.editList(elements[2])
-            e.stopPropagation();
-        })
-        elements[3].addEventListener('click', (e) => {
-            listOfTasks.deleteList(elements[3])
-            e.stopPropagation();
-        })
-        elements[4].addEventListener('click', () => { appLogic.showForm(addItemForm, backgroundOfForms) });
-    }
-
-    static showSelectListContent(list, optionsContent) {
-        const menuContent = list.parentElement.childNodes;
-        const optionsContentChilds = optionsContent.childNodes;
-        menuContent.forEach(item => {
-            if (item.classList.contains('active')) {
-                item.classList.remove('active');
-                optionsContentChilds.forEach(items => { items.classList.remove('activeItemsContainer'); })
-            }
-        })
-        if (!list.classList.contains('active')) {
-            list.classList.add('active');
-            optionsContentChilds.forEach(items => {
-                if (list.classList.contains('active') && list.getAttribute('data-indexlist') == items.getAttribute('data-indexlistitemscontainer')) {
-                    items.classList.add('activeItemsContainer');
-                }
-            })
-        }
-    }
-    static deleteList(element) {
-        const list = element.parentElement.parentElement
-        const index = list.getAttribute('data-indexlist')
-        const items = document.querySelector(`[data-indexlistitemscontainer="${index}"]`)
-        const menuContent = document.querySelector('#menuContent');
-        const optionsContent = document.querySelector('#optionsContent')
-
-        list.remove()
-        items.remove()
-        appLogic.matchingIndex(menuContent, 'data-indexlist')
-        appLogic.matchingIndex(optionsContent, 'data-indexlistitemscontainer')
-        console.log(list)
-        console.log(items)
-    }
-
-    static editList(element) {
-        const editListNameForm = document.querySelector('#editListNameForm');
-        console.log(element)
-        appLogic.showForm(editListNameForm, backgroundOfForms)
+class list {
+    constructor(title, taskCompleted) {
+        this.id;
+        this.title = title;
+        this.tasks = []
+        this.tasksNumber = this.tasks.length;
+        this.taskCompleted = taskCompleted;
+        this.domReference;
     }
 }
 
